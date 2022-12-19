abe7445014f53e334fac39469942a17c8e2934fa
Igal Leikin <igaloly@gmail.com>
2022-12-19 11:19:05 +0200

    fix server

--- pokemon-server/pokemon_server/main.py
+++ pokemon-server/pokemon_server/main.py
@@ -1,34 +1,37 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, parse_obj_as
 import json
 import os
+import uvicorn
 
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 json_path = os.path.join(current_dir, 'data.json')
-print(json_path)
 app = FastAPI()
 
 
-class HeldItem(BaseModel):
+class Evolution(BaseModel):
+    id: str
     name: str
-    description: str
 
 
 class Pokemon(BaseModel):
-    id: int
     name: str
-    image_url: str
     description: str
     types: list[str]
-    abilities: list[str]
-    held_items: list[HeldItem]
+    evolutions: list[Evolution]
+    id: str
+    image_url: str
 
 
-# Open the JSON file
+
 with open(json_path, 'r') as f:
-    # Load the JSON data into a Python variable
-    pokemons = json.load(f)
+    pokemons = parse_obj_as(list[Pokemon], json.load(f))
+
+
+@app.get("/")
+def ping():
+    return "OK"
 
 
 @app.get("/pokemons")
@@ -37,22 +40,12 @@ def get_pokemons():
 
 
 @app.get("/pokemon/{id}")
-def get_pokemon(id: int):
-    if id > len(pokemons):
-        return {"error": f"pokemon id {id} wasn't found"}
-    else:
-        return pokemons[id-1]
-
-
-@app.put("/pokemon/{id}")
-def update_pokemon(id: int, pokemon: Pokemon):
-    if id > len(pokemons):
-        return {"error": f"pokemon id {id} wasn't found"}
-    else:
-        pokemons[id-1] = pokemon
-        return pokemons[id-1]
+def get_pokemon(id: str):
+    found_pokemons = [x for x in pokemons if x.id == id]
+    if len(found_pokemons) == 0:
+        raise HTTPException(status_code=404, detail=f"pokemon id {id} wasn't found")
+    return found_pokemons[0]
 
 
 def run_local():
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=3001)
