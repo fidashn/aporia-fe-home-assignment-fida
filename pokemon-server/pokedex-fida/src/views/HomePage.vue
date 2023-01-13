934414e14fafa012d904b3af1257a60ff9fd9724
fidashn <87206839+fidashn@users.noreply.github.com>
2023-01-14 00:56:56 +0200

    docs:adds a simple input search & creates a structure for the side-bar

---
+++ pokemon-server/pokedex-fida/src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+ <div class="homePage-body">
+  <div class="side-bar">
+    <div class="search-wrapper">
+      <input type="text" placeholder="Type Pokemon Name..." class="input" id="userInput" >
+    </div>
+  </div>
+ </div>
+</template>
