712d89e89c21e270376b4905a5daa97f4795b9ea
fidashn <87206839+fidashn@users.noreply.github.com>
2023-01-14 01:31:08 +0200

    docs:adds styles to the nav & home page

--- pokemon-server/pokedex-fida/src/App.vue
+++ pokemon-server/pokedex-fida/src/App.vue
@@ -12,24 +12,22 @@ function currentDayAndTime() {
 </script>
 
 <template>
-  <header>
-    <img
-      alt="pokedex-logo"
-      class="logo"
-      src="@/assets/Pokédex-logo.png"
-      width="150"
-      height="100"
-    />
-
+     
     <div class="wrapper">
-   <nav>
+      <nav>
+     <img
+       alt="pokedex-logo"
+       class="logo"
+       src="@/assets/Pokédex-logo.png"
+       width="150"
+       height="100"
+     />
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/single-pokemon">Single Pokemon</RouterLink>
         {{ currentDayAndTime() }}
       </nav>
     </div>
-  </header>
-
+ 
   <RouterView />
 </template>
 
@@ -39,5 +37,40 @@ display: flex;
 flex-direction: column;
 height: 100vh;
 width: 100vw;
+}
+
+.wrapper{
+  height:15vh ;
+}
+
+
+nav {
+  display: flex;
+  justify-content:space-around;
+  align-items: center;
+  width: 80vw;
+  font-size: 30px;
+  text-align: center;
+  border: 2px rgb(170, 126, 236) solid;
+  border-radius: 15px;
+  margin-top: 2vw;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
 }
 </style>
